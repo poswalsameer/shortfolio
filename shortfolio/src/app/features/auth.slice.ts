cde02518d29405eabd101632b123c14e1197fc46
poswalsameer <poswalsameer2003@gmail.com>
2024-07-26 20:04:45 +0530

    created store and authSlice

---
+++ shortfolio/src/app/features/auth.slice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: 'authCheck',
+    initialState,
+    reducers: {
+        checkLogin: ( state, action ) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        checkLogout: (state) => {
+            state.status = false;
+        }
+    }
+})
+
+export const { checkLogin, checkLogout } = authSlice.actions;
+
+export default authSlice.reducer;
--- shortfolio/src/app/layout.tsx
+++ shortfolio/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { store } from "./reduxStore/store";
+import { Provider } from "react-redux";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <Provider store={store} >
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </Provider>
   );
 }
