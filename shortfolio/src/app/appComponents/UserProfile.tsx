9ae9e8e313f81fe9c2b58d565d513ad403a9d8c7
poswalsameer <poswalsameer2003@gmail.com>
2024-08-19 20:08:54 +0530

    worked on the ui, next step is to make the social profile section active

--- shortfolio/src/app/appComponents/UserProfile.tsx
+++ shortfolio/src/app/appComponents/UserProfile.tsx
@@ -1,304 +1,266 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { Link2 } from 'lucide-react';
-import { PenLine } from 'lucide-react';
-import { LogOut } from 'lucide-react';
-import Link from 'next/link';
-import authServiceObject from '../appwrite';
-import { useRouter } from 'next/navigation';
-import { checkLogout } from '../features/auth.slice';
-import { useDispatch } from 'react-redux';
-import Cookie from 'js-cookie';
-import databaseServiceObject from '../database.appwrite';
-import { FaSquareXTwitter } from "react-icons/fa6";
+import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Link2 } from "lucide-react";
+import { PenLine } from "lucide-react";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import authServiceObject from "../appwrite";
+import { useRouter } from "next/navigation";
+import { checkLogout } from "../features/auth.slice";
+import { useDispatch } from "react-redux";
+import Cookie from "js-cookie";
+import databaseServiceObject from "../database.appwrite";
+import { FaBehance, FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaReddit } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { Boxes } from "@/components/ui/background-boxes";
+import { cn } from "@/lib/utils";
 
+function page({ params }: { params: any }) {
+  const [error, setError] = useState("");
 
+  const [userDetails, setUserDetails] = useState<any>({});
+  const [userImage, setUserImage] = useState<string>("");
 
-function page({params}:{params:any}) {
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-    const [error, setError] = useState('');
+  const logoutButtonClicked = async () => {
+    setError("");
 
-    const [userDetails, setUserDetails] = useState<any>({});
-    const [userImage, setUserImage] = useState<string>('');
+    try {
+      await authServiceObject.logoutUser();
+      dispatch(checkLogout());
 
-    const dispatch = useDispatch();
-    const router = useRouter();
+      Cookie.remove("userCookie");
 
-    const logoutButtonClicked = async () => {
+      // DELETING COOKIE AFTER LOGGING OUT THE USER
+      const cookieAfterDeletion = Cookie.get("userCookie");
+      console.log("value of cookie after deleting is: ", cookieAfterDeletion);
+      // AFTER LOGGING OUT, THE COOKIE BECOMES UNDEFINED
 
-        setError('');
+      router.push("/login");
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
 
-        try {
-            
-            await authServiceObject.logoutUser();
-            dispatch(checkLogout());
+  // FUNCTION TO CONVERT EMAIL TO NORMAL STRING
+  const convertEmailToString = (data: any) => {
+    let n = data.length;
+    let convertedString = "";
 
-            Cookie.remove('userCookie');
-
-            // DELETING COOKIE AFTER LOGGING OUT THE USER
-            const cookieAfterDeletion = Cookie.get('userCookie');
-            console.log("value of cookie after deleting is: ", cookieAfterDeletion);
-            // AFTER LOGGING OUT, THE COOKIE BECOMES UNDEFINED
-
-            router.push('/login');
-
-        } catch (error: any) {
-            setError(error.message);
-        }
-
+    for (let i = 0; i < n; i++) {
+      if (data[i] === "@" || data[i] === ".") {
+        convertedString = convertedString + "0";
+      } else {
+        convertedString = convertedString + data[i];
+      }
     }
 
-    // FUNCTION TO CONVERT EMAIL TO NORMAL STRING
-    const convertEmailToString = (data: any) => {
+    return convertedString;
+  };
 
-        let n = data.length;
-        let convertedString = "";
+  const getFileID = async (fileId: string) => {
+    try {
+      const file = await databaseServiceObject.getFilePreview(fileId);
 
-        for(let i=0; i<n; i++){
-
-            if( data[i] === '@' || data[i] === '.' ){
-            convertedString = convertedString + '0';
-            }
-            else{
-            convertedString = convertedString + data[i];
-            }
-
-        }
-
-        return convertedString;
-
+      if (file) {
+        console.log("Got the file from the backend");
+        return file;
+      } else {
+        console.log("Didn't got the file from the backend");
+        return undefined;
+      }
+    } catch (error) {
+      console.log(
+        "Error from the frontend side while getting the file: ",
+        error
+      );
     }
+  };
 
-    const getFileID = async ( fileId:string ) => {
+  const getCurrentUserDetails = async () => {
+    try {
+      const currentUser = await authServiceObject.getLoggedInUser();
 
-        try {
-            
-            const file = await databaseServiceObject.getFilePreview(fileId);
+      if (currentUser) {
+        console.log("Details of the current user: ", currentUser);
 
-            if( file ){
-                console.log("Got the file from the backend");
-                return file;
-            }
-            else{
-                console.log("Didn't got the file from the backend");
-                return undefined;
-            }
+        // Converting the email to normal string
+        const convertedEmail = convertEmailToString(currentUser.email);
+        console.log("Converted mail: ", convertedEmail);
 
-        } catch (error) {
-            console.log("Error from the frontend side while getting the file: ", error);
-            
+        //find document with string mail id
+        const userEmail = await databaseServiceObject.getUser(convertedEmail);
+        const photoFile = await getFileID(userEmail.profilePhoto);
+
+        if (photoFile) {
+          setUserImage(photoFile);
         }
+        console.log("The file we got is: ", photoFile);
 
-    }
-
-    const getCurrentUserDetails = async () => {
-
-        try {
-            
-            const currentUser = await authServiceObject.getLoggedInUser();
-
-            if( currentUser ){
-                console.log( "Details of the current user: ", currentUser );
-
-                // TODO: If current user is found, then first convert the email to string, then find for the document with the userEmail string id
-
-                // Converting the email to normal string
-                const convertedEmail = convertEmailToString(currentUser.email);
-                console.log("Converted mail: ", convertedEmail);
-
-                //find document with string mail id
-                const userEmail = await databaseServiceObject.getUser(convertedEmail);
-                const photoFile = await getFileID( userEmail.profilePhoto );
-
-                if( photoFile ){
-                    setUserImage(photoFile);
-                }
-                console.log("The file we got is: ", photoFile);
-                
-
-                if( userEmail ){
-                    console.log("Details of the document with this id: ", userEmail);
-                    setUserDetails(userEmail);
-                    // setUserDetails({...userEmail, photoFile})
-                }
-                else{
-                    setUserDetails({});
-                    console.log("Document not found with this id");
-                }
-                
-            }
-            else{
-                console.log("User details not found");
-            }
-
-        } catch (error) {
-            console.log("Error finding the details of the user: ", error);
-            
+        if (userEmail) {
+          console.log("Details of the document with this id: ", userEmail);
+          setUserDetails(userEmail);
+          // setUserDetails({...userEmail, photoFile})
+        } else {
+          setUserDetails({});
+          console.log("Document not found with this id");
         }
-
+      } else {
+        console.log("User details not found");
+      }
+    } catch (error) {
+      console.log("Error finding the details of the user: ", error);
     }
+  };
 
-    useEffect( () => {
-
-        getCurrentUserDetails();
-        
-    }, [] )  
+  useEffect(() => {
+    getCurrentUserDetails();
+  }, []);
 
   return (
     // <div>
     //   {params.userProfile}
     // </div>
 
-    <div className=' h-screen w-screen flex flex-row justify-center items-center gap-x-10 bg-[#FFF6F2] text-black'>
+    <div className=" h-screen w-screen flex flex-row justify-center items-center gap-x-10 text-black" id="bodyDiv">
+      {/* LEFT SIDE WALA DIV */}
+      <div className="h-[92%] w-[40%] bg-[#D2BFAA] flex flex-col rounded-r-3xl " id="leftDiv">
+        {/* profile photo + name + bio wala div */}
+        <div className="h-[65%] w-full  flex flex-col justify-center items-center gap-y-5 rounded-tr-3xl">
+          {/* IMAGE */}
+          <div className=" h-60 w-60 rounded-full ">
+            <img
+              src={userImage}
+              alt=""
+              className="h-full w-full rounded-full"
+            />
+          </div>
 
-        {/* LEFT SIDE WALA DIV */}
-        <div className='h-[92%] w-[40%] bg-red-500 flex flex-col rounded-r-3xl '>
+          {/* NAME */}
+          <div className=" w-[80%] text-3xl font-bold flex justify-center items-center">
+            {userDetails.fullName}
+          </div>
 
-            {/* profile photo + name + bio wala div */}
-            <div className='h-[65%] w-full bg-red-300 flex flex-col justify-center items-center gap-y-5 rounded-tr-3xl'>
-
-                {/* IMAGE */}
-                <div className=' h-60 w-60 rounded-full '>
-                    <img src={userImage} alt="" className='h-full w-full rounded-full'/>
-                </div>
-
-                {/* NAME */}
-                <div className=' w-[80%] text-3xl font-bold flex justify-center items-center'>
-                    {userDetails.fullName}
-                </div>
-
-                {/* BIO */}
-                <div className='w-[80%] text-lg text-center text-gray-400 font-bold flex justify-center items-center'>
-                   {userDetails.bio}
-                </div>
-
-
-            </div>
-
-            {/* extra link wala div */}
-            <div className='h-[20%] w-full bg-blue-200 flex flex-col justify-center items-center gap-y-3'>
-
-            </div>
-
-            {/* button wala div */}
-            <div className='h-[15%] w-full bg-yellow-100 flex flex-row justify-center items-center gap-x-10 rounded-br-3xl'>
-
-            {/* COPY BUTTON */}
-            <Button variant="secondary" className=' w-32 font-bold flex flex-row justify-center items-center gap-x-2'>
-                Copy
-                <Link2 />
-            </Button>
-
-            {/* EDIT BUTTON */}
-            <Link href='/enterDetails'>
-                <Button variant="destructive" className='w-32 font-bold flex flex-row justify-center items-center gap-x-2'>
-                    Edit
-                    <PenLine />
-                </Button>
-            </Link>
-
-            <Button variant="secondary" className='w-32 font-bold flex flex-row justify-center items-center gap-x-2' onClick={logoutButtonClicked}>
-                    Logout
-                    <LogOut />
-            </Button>
-
-            </div>
-                
+          {/* BIO */}
+          <div className="w-[80%] text-lg text-center text-gray-400 font-bold flex justify-center items-center">
+            {userDetails.bio}
+          </div>
         </div>
 
-        {/* RIGHT SIDE WALA DIV */}
-        <div className='h-[92%] w-[65%] bg-green-500 rounded-l-3xl flex flex-col justify-center items-center gap-y-4 text-white'>
+        {/* extra link wala div */}
+        <div className="h-[20%] w-full flex flex-col justify-center items-center gap-y-3"></div>
 
-            {/* twitter github wala div */}
-            <div className='h-[21.5%] w-[95%] flex justify-center items-center gap-x-5 '>
+        {/* button wala div */}
+        <div className="h-[15%] w-full flex flex-row justify-center items-center gap-x-10 rounded-br-3xl">
+          {/* COPY BUTTON */}
+          <Button
+            variant="secondary"
+            className=" w-32 font-semibold flex flex-row justify-center items-center gap-x-2 transition-all delay-75 ease-in bg-black hover:bg-gray-700"
+          >
+            Copy
+            <Link2 className="h-5 w-5"/>
+          </Button>
 
-                {/* twitter wala div */}
-                <div className='h-full w-[50%] flex justify-center items-center rounded-xl bg-black'>
+          {/* EDIT BUTTON */}
+          <Link href="/enterDetails">
+            <Button
+              variant="destructive"
+              className="w-32 text-sm font-semibold flex flex-row justify-center items-center gap-x-2 transition-all delay-75 ease-in bg-gray-700 hover:bg-gray-800"
+            >
+              Edit
+              <PenLine className="h-5 w-5"/>
+            </Button>
+          </Link>
 
-                    <div className='h-full w-[40%] flex justify-center items-center'>
-                        <FaSquareXTwitter className='h-16 w-16'/>
-                    </div>
-                    
-                    <div className='h-full w-[60%] flex justify-start items-center'>
-                        {userDetails.twitterURL}
-                    </div>
-                    
-                </div>
-                
-                {/* github wala div */}
-                <div className='h-full w-[50%] flex justify-center items-center rounded-xl bg-slate-500'>
+          <Button
+            variant="secondary"
+            className="w-32 text-sm font-semibold flex flex-row justify-center items-center gap-x-2 transition-all delay-75 ease-in bg-blue-900 hover:bg-blue-950"
+            onClick={logoutButtonClicked}
+          >
+            Logout
+            <LogOut className="h-5 w-5"/>
+          </Button>
+        </div>
+      </div>
 
-                    <div className='h-full w-[40%] flex justify-center items-center'>
-                        <FaGithub className='h-16 w-16' />
-                    </div>
-                    
-                    <div className='h-full w-[60%] flex justify-start items-center'>
-                        {userDetails.twitterURL}
-                    </div>
-                    
-                </div>
-
+      {/* RIGHT SIDE WALA DIV */}
+      <div className="h-[92%] w-[65%] rounded-l-3xl flex flex-col justify-center items-center gap-y-4 text-white" id="rightDiv">
+        {/* twitter github wala div */}
+        <div className="h-[21.5%] w-[95%] flex justify-center items-center gap-x-5 ">
+          {/* twitter wala div */}
+          <div className="h-full w-[50%] flex justify-center items-center rounded-xl bg-black">
+            <div className="h-full w-[40%] flex justify-center items-center">
+              <FaSquareXTwitter className="h-16 w-16" />
             </div>
 
-            {/* linkedin wala div */}
-            <div className='h-[21.5%] w-[95%] flex justify-center items-center bg-blue-600 rounded-xl'>
+            <div className="h-full w-[60%] flex justify-start items-center">
+              {userDetails.twitterURL}
+            </div>
+          </div>
 
-                <div className='h-full w-[40%] flex justify-center items-center'>
-                    <FaLinkedin className='h-16 w-16'/>
-                </div>
-                    
-                <div className='h-full w-[60%] flex justify-start items-center'>
-                    {userDetails.linkedinURL}
-                </div>
-                
+          {/* github wala div */}
+          <div className="h-full w-[50%] flex justify-center items-center rounded-xl bg-[#25292F]">
+            <div className="h-full w-[40%] flex justify-center items-center">
+              <FaGithub className="h-16 w-16" />
             </div>
 
-            {/* behance instagram wala div */}
-            <div className='h-[21.5%] w-[95%] flex justify-center items-center gap-x-5 rounded-xl'>
-
-                {/* behance wala div */}
-                <div className='h-full w-[33%] flex justify-center items-center rounded-xl bg-orange-600'>
-
-                    <div className='h-full w-[40%] flex justify-center items-center'>
-                        <FaReddit className='h-16 w-16'/>
-                    </div>
-                        
-                    <div className='h-full w-[60%] flex justify-start items-center'>
-                        {userDetails.behanceURL}
-                    </div>
-                    
-                </div>
-
-                {/* instagram wala div */}
-                <div className='h-full w-[67%] flex justify-center items-center rounded-xl bg-purple-600'>
-
-                    <div className='h-full w-[40%] flex justify-center items-center'>
-                        <FaInstagram className='h-16 w-16'/>
-                    </div>
-                        
-                    <div className='h-full w-[60%] flex justify-start items-center'>
-                        {userDetails.instagramURL}
-                    </div>
-                    
-                </div>
-
+            <div className="h-full w-[60%] flex justify-start items-center">
+              {userDetails.githubURL}
             </div>
-
-            {/* textarea wala div */}
-            <div className='h-[21.5%] w-[95%] flex justify-center items-center bg-black rounded-xl'>
-                {userDetails.TextArea}
-            </div>
-            
+          </div>
         </div>
 
+        {/* linkedin wala div */}
+        <div className="h-[21.5%] w-[95%] flex justify-center items-center bg-[#086BC9] rounded-xl">
+          <div className="h-full w-[40%] flex justify-center items-center">
+            <FaLinkedin className="h-16 w-16" />
+          </div>
+
+          <div className="h-full w-[60%] flex justify-start items-center">
+            {userDetails.linkedinURL}
+          </div>
+        </div>
+
+        {/* behance instagram wala div */}
+        <div className="h-[21.5%] w-[95%] flex justify-center items-center gap-x-5 rounded-xl">
+          {/* behance wala div */}
+          <div className="h-full w-[40%] flex justify-center items-center rounded-xl bg-[#0057FF]">
+            <div className="h-full w-[40%] flex justify-center items-center">
+              <FaBehance className="h-16 w-16" />
+            </div>
+
+            <div className="h-full w-[60%] flex justify-start items-center">
+              {userDetails.behanceURL}
+            </div>
+          </div>
+
+          {/* instagram wala div */}
+          <div className="h-full w-[67%] flex justify-center items-center rounded-xl bg-[#FF7D43]">
+            <div className="h-full w-[40%] flex justify-center items-center">
+              <FaInstagram className="h-16 w-16" />
+            </div>
+
+            <div className="h-full w-[60%] flex justify-start items-center">
+              {userDetails.instagramURL}
+            </div>
+          </div>
+        </div>
+
+        {/* textarea wala div */}
+        <div className='h-[21.5%] w-[95%] flex justify-center items-center bg-black rounded-xl'>
+            {userDetails.TextArea}
+        </div>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
