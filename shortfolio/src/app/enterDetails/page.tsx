6c233452cffd2f4eed4ce7eeff307568919afcda
poswalsameer <poswalsameer2003@gmail.com>
2024-08-24 21:38:27 +0530

    test commit after reset and branch change

--- shortfolio/src/app/enterDetails/page.tsx
+++ shortfolio/src/app/enterDetails/page.tsx
@@ -216,7 +216,7 @@ function page() {
     else{
 
         // console.log("User not found with the same username");
-        
+        //test commit
 
       const updatedUserDetails = await databaseServiceObject.updateUserDetails({
         usernameFrontend: data.username,
